dc8a77a7ff615d4726750abefe641e7d0e511443
qiayuanl <qiayuanl@berkeley.edu>
2025-03-01 02:17:37 -0800

    Fix problem in rewards

--- source/whole_body_tracking/whole_body_tracking/tasks/locomotion/tracking/mdp/rewards.py
+++ source/whole_body_tracking/whole_body_tracking/tasks/locomotion/tracking/mdp/rewards.py
@@ -86,44 +86,49 @@ def motion_global_body_ang_vel_exp(env: ManagerBasedRLEnv, command_name: str, st
 
 
 def motion_relative_body_position_error(env: ManagerBasedRLEnv, command_name: str,
-                                        body_names: list[str] | None) -> torch.Tensor:  # TODO doesn't work
+                                        body_names: Optional[list[str]] = None) -> torch.Tensor:
     command: MotionCommand = env.command_manager.get_term(command_name)
 
     if body_names is None:
         body_names = command.cfg.body_names
     body_indexes = [i for i, body_name in enumerate(command.cfg.body_names) if
                     body_name in body_names]  # TODO hurt performance?
+    num_bodies = len(body_indexes)
     relative_pos_motion, _ = subtract_frame_transforms(
-        command.motion_ref_pose_w[:, :3], command.motion_ref_pose_w[:, 3:7],
+        command.motion_ref_pose_w[:, None, :3].repeat(1, num_bodies, 1),
+        command.motion_ref_pose_w[:, None, 3:7].repeat(1, num_bodies, 1),
         command.motion_body_pose_w[:, body_indexes, :3]
     )
     relative_pos_robot, _ = subtract_frame_transforms(
-        command.robot_ref_pose_w[:, :3], command.robot_ref_pose_w[:, 3:7],
+        command.robot_ref_pose_w[:, None, :3].repeat(1, num_bodies, 1),
+        command.robot_ref_pose_w[:, None, 3:7].repeat(1, num_bodies, 1),
         command.robot_body_pose_w[:, body_indexes, :3]
     )
     return (relative_pos_motion - relative_pos_robot).pow(2).mean(-1).mean(-1)
 
 
 def motion_relative_body_position_error_exp(
-        env: ManagerBasedRLEnv, std: float, command_name: str, body_names: Optional[list[str]] = None
-) -> torch.Tensor:
+        env: ManagerBasedRLEnv, std: float, command_name: str, body_names: Optional[list[str]] = None) -> torch.Tensor:
     return torch.exp(-motion_relative_body_position_error(env, command_name, body_names) * std)
 
 
 def motion_relative_body_orientation_error(env: ManagerBasedRLEnv, command_name: str,
-                                           body_names: Optional[list[str]] = None) -> torch.Tensor:  # TODO doesn't work
+                                           body_names: Optional[list[str]] = None) -> torch.Tensor:
     command: MotionCommand = env.command_manager.get_term(command_name)
 
     if body_names is None:
         body_names = command.cfg.body_names
     body_indexes = [i for i, body_name in enumerate(command.cfg.body_names) if
                     body_name in body_names]  # TODO hurt performance?
+    num_bodies = len(body_indexes)
     _, relative_ori_motion = subtract_frame_transforms(
-        command.motion_ref_pose_w[:, :3], command.motion_ref_pose_w[:, 3:7], None,
+        command.motion_ref_pose_w[:, None, :3].repeat(1, num_bodies, 1),
+        command.motion_ref_pose_w[:, None, 3:7].repeat(1, num_bodies, 1), None,
         command.motion_body_pose_w[:, body_indexes, 3:7]
     )
     _, relative_ori_robot = subtract_frame_transforms(
-        command.robot_ref_pose_w[:, :3], command.robot_ref_pose_w[:, 3:7], None,
+        command.robot_ref_pose_w[:, None, :3].repeat(1, num_bodies, 1),
+        command.robot_ref_pose_w[:, None, 3:7].repeat(1, num_bodies, 1), None,
         command.robot_body_pose_w[:, body_indexes, 3:7]
     )
     return quat_error_magnitude(relative_ori_motion, relative_ori_robot).pow(2).mean(-1).mean(-1)
@@ -134,12 +139,15 @@ def motion_relative_body_lin_vel_exp(env: ManagerBasedRLEnv, command_name: str, 
     if body_names is None:
         body_names = command.cfg.body_names
     body_indexes = [i for i, body_name in enumerate(command.cfg.body_names) if body_name in body_names]
+    num_bodies = len(body_indexes)
     relative_vel_motion, _ = subtract_frame_transforms(
-        command.motion_ref_pose_w[:, :3], command.motion_ref_pose_w[:, 3:7],
+        command.motion_ref_pose_w[:, None, :3].repeat(1, num_bodies, 1),
+        command.motion_ref_pose_w[:, None, 3:7].repeat(1, num_bodies, 1),
         command.robot_body_vel_w[:, body_indexes, 7:10]
     )
     relative_vel_robot, _ = subtract_frame_transforms(
-        command.robot_ref_pose_w[:, :3], command.robot_ref_pose_w[:, 3:7],
+        command.robot_ref_pose_w[:, None, :3].repeat(1, num_bodies, 1),
+        command.robot_ref_pose_w[:, None, 3:7].repeat(1, num_bodies, 1),
         command.robot_body_vel_w[:, body_indexes, :3]
     )
     return torch.exp(-((relative_vel_motion - relative_vel_robot).pow(2).mean(-1).mean(-1)) * std)
@@ -151,12 +159,15 @@ def motion_relative_body_ang_vel_exp(env: ManagerBasedRLEnv, command_name: str, 
     if body_names is None:
         body_names = command.cfg.body_names
     body_indexes = [i for i, body_name in enumerate(command.cfg.body_names) if body_name in body_names]
+    num_bodies = len(body_indexes)
     relative_ang_vel_motion, _ = subtract_frame_transforms(
-        command.motion_ref_pose_w[:, :3], command.motion_ref_pose_w[:, 3:7],
+        command.motion_ref_pose_w[:, None, :3].repeat(1, num_bodies, 1),
+        command.motion_ref_pose_w[:, None, 3:7].repeat(1, num_bodies, 1),
         command.motion_body_vel_w[:, body_indexes, 3:6]
     )
     relative_ang_vel_robot, _ = subtract_frame_transforms(
-        command.robot_ref_pose_w[:, :3], command.robot_ref_pose_w[:, 3:7],
+        command.robot_ref_pose_w[:, None, :3].repeat(1, num_bodies, 1),
+        command.robot_ref_pose_w[:, None, 3:7].repeat(1, num_bodies, 1),
         command.robot_body_vel_w[:, body_indexes, 3:6]
     )
     return torch.exp(-((relative_ang_vel_motion - relative_ang_vel_robot).pow(2).mean(-1).mean(-1)) * std)
@@ -164,11 +175,14 @@ def motion_relative_body_ang_vel_exp(env: ManagerBasedRLEnv, command_name: str, 
 
 def motion_joint_pos_error(env: ManagerBasedRLEnv, command_name: str) -> torch.Tensor:
     command: MotionCommand = env.command_manager.get_term(command_name)
+
     return torch.norm(command.motion_joint_pos - command.robot_joint_pos, dim=1)
 
 
 def motion_joint_pos_error_exp(env: ManagerBasedRLEnv, command_name: str, std: float) -> torch.Tensor:
-    return torch.exp(-motion_joint_pos_error(env, command_name) / std ** 2)
+    command: MotionCommand = env.command_manager.get_term(command_name)
+
+    return torch.exp(torch.square(command.motion_joint_pos - command.robot_joint_pos) / std ** 2)
 
 
 def motion_joint_vel_error(env: ManagerBasedRLEnv, command_name: str) -> torch.Tensor:
