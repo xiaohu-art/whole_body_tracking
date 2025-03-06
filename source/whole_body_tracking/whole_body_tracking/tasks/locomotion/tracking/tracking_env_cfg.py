15fe7de38d67e1b8affa97195d86e93d9c0c2055
qiayuanl <qiayuanl@berkeley.edu>
2025-03-06 11:41:24 -0800

    Add domain randomization and noise for observation

--- source/whole_body_tracking/whole_body_tracking/tasks/locomotion/tracking/tracking_env_cfg.py
+++ source/whole_body_tracking/whole_body_tracking/tasks/locomotion/tracking/tracking_env_cfg.py
@@ -18,6 +18,7 @@ from isaaclab.terrains import TerrainImporterCfg
 ##
 from isaaclab.utils import configclass
 import whole_body_tracking.tasks.locomotion.tracking.mdp as mdp
+from isaaclab.utils.noise import AdditiveUniformNoiseCfg as Unoise
 
 
 ##
@@ -88,13 +89,16 @@ class ObservationsCfg:
 
         # observation terms (order preserved)
         command = ObsTerm(func=mdp.generated_commands, params={"command_name": "motion"})
-        ref_ori_w = ObsTerm(func=mdp.robot_ref_ori_w, params={"command_name": "motion"})
-        body_pos = ObsTerm(func=mdp.robot_body_pos_b, params={"command_name": "motion"})
-        body_ori = ObsTerm(func=mdp.robot_body_ori_b, params={"command_name": "motion"})
-        base_lin_vel = ObsTerm(func=mdp.base_lin_vel)
-        base_ang_vel = ObsTerm(func=mdp.base_ang_vel)
-        joint_pos = ObsTerm(func=mdp.joint_pos_rel)
-        joint_vel = ObsTerm(func=mdp.joint_vel_rel)
+        ref_ori_w = ObsTerm(func=mdp.robot_ref_ori_w, params={"command_name": "motion"},
+                            noise=Unoise(n_min=-0.05, n_max=0.05))
+        body_pos = ObsTerm(func=mdp.robot_body_pos_b, params={"command_name": "motion"},
+                           noise=Unoise(n_min=-0.05, n_max=0.05))
+        body_ori = ObsTerm(func=mdp.robot_body_ori_b, params={"command_name": "motion"},
+                           noise=Unoise(n_min=-0.05, n_max=0.05))
+        base_lin_vel = ObsTerm(func=mdp.base_lin_vel, noise=Unoise(n_min=-0.1, n_max=0.1))
+        base_ang_vel = ObsTerm(func=mdp.base_ang_vel, noise=Unoise(n_min=-0.2, n_max=0.2))
+        joint_pos = ObsTerm(func=mdp.joint_pos_rel, noise=Unoise(n_min=-0.01, n_max=0.01))
+        joint_vel = ObsTerm(func=mdp.joint_vel_rel, noise=Unoise(n_min=-0.5, n_max=0.5))
         actions = ObsTerm(func=mdp.last_action)
 
         def __post_init__(self):
@@ -115,13 +119,32 @@ class EventCfg:
         mode="startup",
         params={
             "asset_cfg": SceneEntityCfg("robot", body_names=".*"),
-            "static_friction_range": (0.8, 0.8),
-            "dynamic_friction_range": (0.6, 0.6),
-            "restitution_range": (0.0, 0.0),
+            "static_friction_range": (0.3, 0.8),
+            "dynamic_friction_range": (0.3, 0.6),
+            "restitution_range": (0.0, 0.5),
             "num_buckets": 64,
         },
     )
 
+    add_base_mass = EventTerm(
+        func=mdp.randomize_rigid_body_mass,
+        mode="startup",
+        params={
+            "asset_cfg": SceneEntityCfg("robot", body_names="torso_link"),
+            "mass_distribution_params": (-3.0, 3.0),
+            "operation": "add",
+        },
+    )
+
+    scale_all_link_masses = EventTerm(
+        func=mdp.randomize_rigid_body_mass,
+        mode="startup",
+        params={
+            "asset_cfg": SceneEntityCfg("robot", body_names=".*"),
+            "mass_distribution_params": (0.9, 1.1),
+            "operation": "scale",
+        },
+    )
 
 @configclass
 class RewardsCfg:
