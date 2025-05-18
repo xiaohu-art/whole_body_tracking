5615e23c2b2b89a2ebca1fd23a24013c08db8fb2
qiayuanl <qiayuanl@berkeley.edu>
2025-05-18 01:08:55 -0700

    Reduced observation (antipodal quaternions problem, should use rotation matrix), significant better in jumping sim2sim!

--- source/whole_body_tracking/whole_body_tracking/tasks/tracking/tracking_env_cfg.py
+++ source/whole_body_tracking/whole_body_tracking/tasks/tracking/tracking_env_cfg.py
@@ -97,12 +97,7 @@ class ObservationsCfg:
         command = ObsTerm(func=mdp.generated_commands, params={"command_name": "motion"})
         motion_pos_b = ObsTerm(func=mdp.motion_ref_pos_b, params={"command_name": "motion"},
                                noise=Unoise(n_min=-0.05, n_max=0.05))
-        robot_ori_w = ObsTerm(func=mdp.robot_ref_ori_w, params={"command_name": "motion"},
-                            noise=Unoise(n_min=-0.05, n_max=0.05))
-        body_pos = ObsTerm(func=mdp.robot_body_pos_b, params={"command_name": "motion"},
-                           noise=Unoise(n_min=-0.05, n_max=0.05))
-        body_ori = ObsTerm(func=mdp.robot_body_ori_b, params={"command_name": "motion"},
-                           noise=Unoise(n_min=-0.05, n_max=0.05))
+        projected_gravity = ObsTerm(func=mdp.projected_gravity, noise=Unoise(n_min=-0.05, n_max=0.05))
         base_lin_vel = ObsTerm(func=mdp.base_lin_vel, noise=Unoise(n_min=-0.5, n_max=0.5))
         base_ang_vel = ObsTerm(func=mdp.base_ang_vel, noise=Unoise(n_min=-0.2, n_max=0.2))
         joint_pos = ObsTerm(func=mdp.joint_pos_rel, noise=Unoise(n_min=-0.01, n_max=0.01))
