ea25e6a1829afb5b1ef249f47add7ca0297480a3
qiayuanl <liaoqiayuan@gmail.com>
2025-07-02 22:44:54 -0400

    Update the export onnx name while training

--- source/whole_body_tracking/whole_body_tracking/utils/my_on_policy_runner.py
+++ source/whole_body_tracking/whole_body_tracking/utils/my_on_policy_runner.py
@@ -17,10 +17,11 @@ class MyOnPolicyRunner(OnPolicyRunner):
         super().save(path, infos)
         if self.logger_type in ["wandb"]:
             policy_path = path.split('model')[0]
+            filename = policy_path.split('/')[-2] + ".onnx"
             export_motion_policy_as_onnx(self.env.unwrapped, self.alg.actor_critic, normalizer=self.obs_normalizer,
-                                         path=policy_path)
-            attach_onnx_metadata(self.env.unwrapped, wandb.run.name, path=policy_path)
-            wandb.save(policy_path + "policy.onnx", base_path=os.path.dirname(policy_path))
+                                         path=policy_path, filename=filename)
+            attach_onnx_metadata(self.env.unwrapped, wandb.run.name, path=policy_path, filename=filename)
+            wandb.save(policy_path + filename, base_path=os.path.dirname(policy_path))
 
             # link the artifact registry to this run
             if self.registry_name is not None:
