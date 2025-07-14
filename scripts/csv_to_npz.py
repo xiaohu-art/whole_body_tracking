d30c86a48f7a7b08ef5e9026519a78ea11d2740b
qiayuanl <liaoqiayuan@gmail.com>
2025-07-13 21:42:59 -0400

    Add printing for wandb motion registration

--- scripts/csv_to_npz.py
+++ scripts/csv_to_npz.py
@@ -321,10 +321,11 @@ def run_simulator(
             import wandb
             COLLECTION = args_cli.output_name
             run = wandb.init(project="csv_to_npz", name=COLLECTION)
+            print(f"[INFO]: Logging motion to wandb: {COLLECTION}")
             REGISTRY = "motions"
             logged_artifact = run.log_artifact(artifact_or_path="/tmp/motion.npz", name=COLLECTION, type=REGISTRY)
             run.link_artifact(artifact=logged_artifact, target_path=f"wandb-registry-{REGISTRY}/{COLLECTION}")
-
+            print(f"[INFO]: Motion saved to wandb registry: {REGISTRY}/{COLLECTION}")
 
 def main():
     """Main function."""
