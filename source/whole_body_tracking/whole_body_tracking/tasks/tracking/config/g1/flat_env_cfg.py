3cac3cc05c4cc8ce5167cca7a637cb76db02a421
qiayuanl <liaoqiayuan@gmail.com>
2025-07-24 21:55:45 -0400

    Add missing action_scale of g1

--- source/whole_body_tracking/whole_body_tracking/tasks/tracking/config/g1/flat_env_cfg.py
+++ source/whole_body_tracking/whole_body_tracking/tasks/tracking/config/g1/flat_env_cfg.py
@@ -1,5 +1,5 @@
 from whole_body_tracking.assets import ASSET_DIR
-from whole_body_tracking.robots.g1 import G1_CYLINDER_CFG
+from whole_body_tracking.robots.g1 import G1_CYLINDER_CFG, G1_ACTION_SCALE
 from whole_body_tracking.tasks.tracking.tracking_env_cfg import TrackingEnvCfg
 
 from isaaclab.utils import configclass
@@ -11,7 +11,7 @@ class G1FlatEnvCfg(TrackingEnvCfg):
         super().__post_init__()
 
         self.scene.robot = G1_CYLINDER_CFG.replace(prim_path="{ENV_REGEX_NS}/Robot")
-
+        self.actions.joint_pos.scale = G1_ACTION_SCALE
         self.commands.motion.reference_body = "torso_link"
         self.commands.motion.body_names = ['pelvis',
                                            'left_hip_roll_link', 'left_knee_link', 'left_ankle_roll_link',
