1102f84321ed1159aa4569757325ba4187109bd7
qiayuanl <liaoqiayuan@gmail.com>
2025-07-24 20:19:44 -0400

    Remove redundant observation

--- source/whole_body_tracking/whole_body_tracking/tasks/tracking/tracking_env_cfg.py
+++ source/whole_body_tracking/whole_body_tracking/tasks/tracking/tracking_env_cfg.py
@@ -113,8 +113,6 @@ class ObservationsCfg:
                                    noise=Unoise(n_min=-0.25, n_max=0.25))
         motion_ref_ori_b = ObsTerm(func=mdp.motion_ref_ori_b, params={"command_name": "motion"},
                                    noise=Unoise(n_min=-0.05, n_max=0.05))
-        motion_ref_ori_b = ObsTerm(func=mdp.motion_ref_ori_b, params={"command_name": "motion"},
-                                   noise=Unoise(n_min=-0.05, n_max=0.05))
         base_lin_vel = ObsTerm(func=mdp.base_lin_vel, noise=Unoise(n_min=-0.5, n_max=0.5))
         base_ang_vel = ObsTerm(func=mdp.base_ang_vel, noise=Unoise(n_min=-0.2, n_max=0.2))
         joint_pos = ObsTerm(func=mdp.joint_pos_rel, noise=Unoise(n_min=-0.01, n_max=0.01))
